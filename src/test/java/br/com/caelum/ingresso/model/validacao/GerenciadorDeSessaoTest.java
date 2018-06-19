2b4be73fe29c38a864d1c47de2f873bafbd2a30f
marcelohmb <marcelo.hmb@gmail.com>
2018-06-19 20:58:54 -0300

    Preco adicionado ao Projeto

--- src/test/java/br/com/caelum/ingresso/model/validacao/GerenciadorDeSessaoTest.java
+++ src/test/java/br/com/caelum/ingresso/model/validacao/GerenciadorDeSessaoTest.java
@@ -2,6 +2,7 @@ package br.com.caelum.ingresso.model.validacao;
 
 import static org.junit.Assert.*;
 
+import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalTime;
 import java.util.Arrays;
@@ -20,12 +21,18 @@ public class GerenciadorDeSessaoTest {
 	private Filme aLagoaAzul;
 	private Sala salaZeroUm;
 	private Sessao sessaoExistente;
+	private Filme rogueOne;
+	private Sala sala3D;
+	private Sessao sessaoDasDez;
+	private Sessao sessaoDasTreze;
+	private Sessao sessaoDasDezoito;
+	
 
 	@Before
 	public void setup() {
 		
-		aLagoaAzul = new Filme("A Lagoa Azul", Duration.ofMinutes(120), "Amor");
-		salaZeroUm = new Sala("Sala 01");
+		aLagoaAzul = new Filme("A Lagoa Azul", Duration.ofMinutes(120), "Amor", new BigDecimal("5.0"));
+		salaZeroUm = new Sala("Sala 01",new BigDecimal("10.0"));
 		sessaoExistente = new Sessao(LocalTime.parse("14:00:00"), aLagoaAzul, salaZeroUm);
 		
 		
@@ -52,5 +59,20 @@ public class GerenciadorDeSessaoTest {
 		
 		assertFalse(coube);
 	}
+	
+	@Before
+	public void preparaSessoes(){
+		
+		this.rogueOne = new Filme("Rogue One", Duration.ofMinutes(120),"SCI-FI",BigDecimal.ONE);
+		this.sala3D = new Sala("Sala 3D", BigDecimal.TEN);
+		
+		this.sessaoDasDez = new Sessao(LocalTime.parse("10:00:00"), rogueOne, sala3D);
+		this.sessaoDasTreze = new Sessao(LocalTime.parse("13:00:00"), rogueOne, sala3D);
+		this.sessaoDasDezoito = new Sessao(LocalTime.parse("18:00:00"), rogueOne, sala3D);
+		
+		
+	}
+	
+	
 
 }

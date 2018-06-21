b7497da7f8072216a5a3baa20b3953ca892e7d85
marcelohmb <marcelo.hmb@gmail.com>
2018-06-20 23:05:32 -0300

    Inicio das Classes Genericas

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -62,5 +63,16 @@ public class SessaoController {
 		else {
 			return form(form.getSalaId(), form);
 		}
+	}
+	
+	@GetMapping("/sessao/{id}/lugares")
+	public ModelAndView lugaresNaSessao(@PathVariable("id") Integer sessaoId){
+		ModelAndView modelAndView = new ModelAndView("sessao/lugares");
+		
+		Sessao sessao = sessaoDao.findOne(sessaoId);
+		
+		modelAndView.addObject("sessao", sessao);
+		
+		return modelAndView;
 	}
 }
